630d0a1843055a52978c1e8da8b3f311a46302f7
vpoquette <vpoquette@css.edu>
2023-12-08 17:36:40 -0600

    added images for rest of ingredients

---
+++ lib/main_simple.dart
@@ -0,0 +1,214 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recipe Simulator',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Recipe Simulator Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+// Current Goal:
+  // cook a pre-set recipe by following the directions
+
+// use Hive to store current progress in recipe
+
+class _MyHomePageState extends State<MyHomePage> {
+  // logic variables, methods
+    // need to move these to their own file for Separation of Concerns
+
+  // six ingredients for our first recipe (bread)
+  int flour = 0; // unit: cups
+  int yeast = 0; // unit: tbsp
+  int water = 0; // unit: cups
+  int sugar = 0; // unit: cups
+  int salt = 0; // unit: tsp
+  int oil = 0; // unit: tbsp
+  // progress checks
+  bool success = false;
+  String message = "";
+
+  // for later recipes
+  int egg = 0; // unit: egg
+  int milk = 0; // unit: cups
+
+  void checkIngredients(){
+    // https://tastesbetterfromscratch.com/bread-recipe/
+    setState(() {
+      if (water == 2 && yeast == 1 && sugar == 1 && salt == 2 && oil == 2 && flour == 4) {
+        success = true;
+      }
+      print(success);
+    });
+  }
+
+  // TODO: add a START OVER button which resets ingredients to 0 if you mess up
+
+  void addFlour(){
+    setState(() {
+      flour ++;
+    });
+  }
+
+  void addYeast(){
+    setState(() {
+      yeast ++;
+    });
+  }
+
+  void addWater(){
+    setState(() {
+      water ++;
+    });
+  }
+
+  void addSugar(){
+    setState(() {
+      sugar ++;
+    });
+  }
+
+  void addSalt(){
+    setState(() {
+      salt ++;
+    });
+  }
+
+  void addOil(){
+    setState(() {
+      oil ++;
+    });
+  }
+
+  void cook(){
+    setState(() {
+      if(success == true){
+        message = "You did it! Congratulations";
+      }
+      else{
+        message = "You made a mistake; better try again.";
+      }
+    });
+  }
+
+  // let's build a user interface
+    // difficult to use an app without one
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title), // draws from one of the first widgets on this page
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // center the UI
+          children: <Widget>[
+            // flour
+            Image(image: AssetImage('graphics/flour_immo_wegmann.jpg'), height: 100),
+            Text("Cups of Flour: $flour", style: Theme.of(context).textTheme.headlineMedium), // header
+            SizedBox(height: 20), // line break
+            ElevatedButton(
+              onPressed: () {
+                addFlour();
+              },
+              child: Text('Add a Cup of Flour'),
+            ),
+            SizedBox(height: 20), // line break
+            // yeast
+            Image(image: AssetImage('graphics/yeast_karyna_panchenko.jpg'), height: 100),
+            Text("Tablespoons of Yeast: $yeast", style: Theme.of(context).textTheme.headlineMedium), // header
+            SizedBox(height: 20), // line break
+            ElevatedButton(
+              onPressed: () {
+                addYeast();
+              },
+              child: Text('Add a Tablespoon of Yeast'),
+            ),
+            SizedBox(height: 20), // line break
+
+            // TODO: find and add images for buttons below
+
+            Text("Cups of Warm Water: $water", style: Theme.of(context).textTheme.headlineMedium),
+            ElevatedButton(
+              onPressed: () {
+                addWater();
+              },
+              child: Text('Add a Cup of Warm Water'),
+            ),
+            SizedBox(height: 20), // line break
+
+            Text("Quarter-Cups of Sugar: $sugar", style: Theme.of(context).textTheme.headlineMedium),
+            ElevatedButton(
+              onPressed: () {
+                addSugar();
+              },
+              child: Text('Add a Quarter-Cup of Sugar'),
+            ),
+            SizedBox(height: 20), // line break
+
+            Text("Teaspoons of Salt: $salt", style: Theme.of(context).textTheme.headlineMedium),
+            ElevatedButton(
+              onPressed: () {
+                addSalt();
+              },
+              child: Text('Add a Teaspoon of Salt'),
+            ),
+            SizedBox(height: 20), // line break
+
+            Text("Tablespoons of Oil: $oil", style: Theme.of(context).textTheme.headlineMedium),
+            ElevatedButton(
+              onPressed: () {
+                addOil();
+              },
+              child: Text('Add a Tablespoon of Oil'),
+            ),
+            SizedBox(height: 20), // line break
+
+            Text("Recipe Success: $success", style: Theme.of(context).textTheme.headlineMedium),
+            Text("$message", style: Theme.of(context).textTheme.headlineMedium),
+            ElevatedButton(
+              onPressed: () {
+                checkIngredients();
+                cook();
+              },
+              child: Text('Cook'),
+            ),
+
+          ],
+        ),
+      ),
+      /*
+      floatingActionButton: FloatingActionButton(
+        onPressed: addFlour,
+        tooltip: 'Add Flour',
+        child: const Icon(Icons.add),
+      ),
+       */
+    );
+  }
+}
--- lib/main_sliver.dart
+++ lib/main_sliver.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'ingredient.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,196 +30,191 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+// Current Goal:
+// cook a pre-set recipe by following the directions
+
+// use Hive to store current progress in recipe
+
 class _MyHomePageState extends State<MyHomePage> {
-  //const HorizonsApp({super.key});
+  // logic variables, methods
+  // need to move these to their own file for Separation of Concerns
 
-  Ingredient flour = new Ingredient(
-      "flour", "flour_immo_wegmann.jpg", 0, "cup");
-  Ingredient yeast = new Ingredient(
-      "yeast", "yeast_karyna_panchenko.jpg", 0, "tbsp");
+  // TODO: add ingredient list to this file
 
-  // need imgs
-  Ingredient water = new Ingredient(
-      "water", "flour_immo_wegmann.jpg", 0, "cup");
-  Ingredient sugar = new Ingredient(
-      "sugar", "flour_immo_wegmann.jpg", 0, "cup");
-  Ingredient salt = new Ingredient("salt", "flour_immo_wegmann.jpg", 0, "tsp");
-  Ingredient oil = new Ingredient("oil", "flour_immo_wegmann.jpg", 0, "tbsp");
-
+  // six ingredients for our first recipe (bread)
+  int flour = 0; // unit: cups
+  int yeast = 0; // unit: tbsp
+  int water = 0; // unit: cups
+  int sugar = 0; // unit: cups
+  int salt = 0; // unit: tsp
+  int oil = 0; // unit: tbsp
+  // progress checks
   bool success = false;
   String message = "";
 
-  increaseIngredient(ingredient) {
-    setState(() { // need this to be in the home page state
-      ingredient.increase(1);
+  // for later recipes
+  int egg = 0; // unit: egg
+  int milk = 0; // unit: cups
+
+  void checkIngredients(){
+    // https://tastesbetterfromscratch.com/bread-recipe/
+    setState(() {
+      if (water == 2 && yeast == 1 && sugar == 1 && salt == 2 && oil == 2 && flour == 4) {
+        success = true;
+      }
+      print(success);
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // doesn't error out here (sweats nervously)
-    availableIngredients.add(flour);
-    availableIngredients.add(yeast);
-    availableIngredients.add(water);
-    availableIngredients.add(sugar);
-    availableIngredients.add(salt);
-    availableIngredients.add(oil);
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      scrollBehavior: const ConstantScrollBehavior(),
-      title: 'Recipe Simulator',
-      home: Scaffold(
-        body: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              //floating: true,
-              pinned: true,
-              stretch: true,
-              //stretchTriggerOffset: 10, // this doesn't seem to help it trigger
-              onStretchTrigger: () async { // this doesn't seem to be triggering
-                print('Load new data!');
-                // await Server.requestNewData();
-              },
-              backgroundColor: Colors.green[500],
-              expandedHeight: 200.0,
-              flexibleSpace: FlexibleSpaceBar(
-                stretchModes: const [
-                  //StretchMode.zoomBackground,
-                  //StretchMode.fadeTitle,
-                  StretchMode.blurBackground,
-                ],
-                title: Text('Recipe Simulator'),
-                background: DecoratedBox(
-                  position: DecorationPosition.foreground,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.bottomCenter,
-                      end: Alignment.center,
-                      colors: <Color>[
-                        Colors.red[700]!,
-                        Colors.transparent,
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            //IngredientList(),
-            IngredientListMethod(),
-          ],
-        ),
-      ),
-    );
+  // TODO: add a START OVER button which resets ingredients to 0 if you mess up
+
+  void addFlour(){
+    setState(() {
+      flour ++;
+    });
   }
-}
 
-Widget IngredientListMethod() {
-  print("in IngredientListMethod with length = "+availableIngredients.length.toString());
-  return SliverList(
-    delegate: SliverChildBuilderDelegate(
-          (BuildContext context, int index) {
-        final Ingredient ingredient = availableIngredients[index]; // grab current ingredient
-        print(availableIngredients.length);
-        return cardWidget(ingredient);
-        //return Text("ingredient is "+ingredient.name);
-      },
-      childCount: availableIngredients.length,
-    ),
-  );
-}
-Widget cardWidget(Ingredient ingredient)  {
+  void addYeast(){
+    setState(() {
+      yeast ++;
+    });
+  }
 
-  // find a spot where these methods won't error out
-  /*
-  void checkIngredients(){
-    // https://tastesbetterfromscratch.com/bread-recipe/
-    if (water.getCount() == 2 && yeast.getCount() == 1 && sugar.getCount() == 1 && salt.getCount() == 2 && oil.getCount() == 2 && flour.getCount() == 4) {
-      success = true;
-    }
-    print(success);
+  void addWater(){
+    setState(() {
+      water ++;
+    });
+  }
+
+  void addSugar(){
+    setState(() {
+      sugar ++;
+    });
+  }
+
+  void addSalt(){
+    setState(() {
+      salt ++;
+    });
+  }
+
+  void addOil(){
+    setState(() {
+      oil ++;
+    });
   }
 
   void cook(){
-    if(success == true){
-      message = "You did it! Congratulations";
-    }
-    else{
-      message = "You made a mistake; better try again.";
-    }
+    setState(() {
+      if(success == true){
+        message = "You did it! Congratulations";
+      }
+      else{
+        message = "You made a mistake; better try again.";
+      }
+    });
   }
-  */
 
-// TODO: add a START OVER button which resets ingredients to 0 if you mess up
+  // let's build a user interface
+  // difficult to use an app without one
+  @override
+  Widget build(BuildContext context) {
 
-  return Card(
-    child: Row(
-        children: <Widget>[
-          Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    // left align
-                    children: <Widget>[
-                      Text(
-                        ingredient.getName() + ": " + ingredient.getCount().toString() + " " + ingredient.getMeasurement(),
-                        //style: textTheme.headlineSmall,
-                      ),
-                      SizedBox( // size of card
-                        height: 200.0,
-                        width: 200.0,
-                        child: Stack(
-                          fit: StackFit.expand,
-                          children: <Widget>[
-                            Center(
-                              child: Image(image: AssetImage(
-                                  'graphics/' + ingredient.getImg()),
-                                fit: BoxFit.fill,),
-                            ),
-                          ],
-                        ),
-                      ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title), // draws from one of the first widgets on this page
+      ),
 
-                      /*
-                      ElevatedButton(
-                        onPressed: () {
-                          increaseIngredient(ingredient);
-                        },
-                        child: Text('Add a ' + ingredient.getMeasurement() + " of " + ingredient.getName()),
-                      ),
-                      */
-                    ]
-                ),
-              )
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, // center the UI
+            children: <Widget>[
+              // flour
+              Image(image: AssetImage('graphics/flour_immo_wegmann.jpg'), height: 100),
+              Text("Cups of Flour: $flour", style: Theme.of(context).textTheme.headlineMedium), // header
+              SizedBox(height: 20), // line break
+              ElevatedButton(
+                onPressed: () {
+                  addFlour();
+                },
+                child: Text('Add a Cup of Flour'),
+              ),
+              SizedBox(height: 20), // line break
+              // yeast
+              Image(image: AssetImage('graphics/yeast_karyna_panchenko.jpg'), height: 100),
+              Text("Tablespoons of Yeast: $yeast", style: Theme.of(context).textTheme.headlineMedium), // header
+              SizedBox(height: 20), // line break
+              ElevatedButton(
+                onPressed: () {
+                  addYeast();
+                },
+                child: Text('Add a Tablespoon of Yeast'),
+              ),
+              SizedBox(height: 20), // line break
+
+              Image(image: AssetImage('graphics/water_tanushree_rao.jpg'), height: 100),
+              Text("Cups of Warm Water: $water", style: Theme.of(context).textTheme.headlineMedium),
+              ElevatedButton(
+                onPressed: () {
+                  addWater();
+                },
+                child: Text('Add a Cup of Warm Water'),
+              ),
+              SizedBox(height: 20), // line break
+
+              Image(image: AssetImage('graphics/sugar_faran-raufi.jpg'), height: 100),
+              Text("Quarter-Cups of Sugar: $sugar", style: Theme.of(context).textTheme.headlineMedium),
+              ElevatedButton(
+                onPressed: () {
+                  addSugar();
+                },
+                child: Text('Add a Quarter-Cup of Sugar'),
+              ),
+              SizedBox(height: 20), // line break
+
+              Image(image: AssetImage('graphics/salt_gidlark.jpg'), height: 100),
+              Text("Teaspoons of Salt: $salt", style: Theme.of(context).textTheme.headlineMedium),
+              ElevatedButton(
+                onPressed: () {
+                  addSalt();
+                },
+                child: Text('Add a Teaspoon of Salt'),
+              ),
+              SizedBox(height: 20), // line break
+
+              Image(image: AssetImage('graphics/oil_roberta-sorge.jpg'), height: 100),
+              Text("Tablespoons of Oil: $oil", style: Theme.of(context).textTheme.headlineMedium),
+              ElevatedButton(
+                onPressed: () {
+                  addOil();
+                },
+                child: Text('Add a Tablespoon of Oil'),
+              ),
+              SizedBox(height: 20), // line break
+
+              Text("Recipe Success: $success", style: Theme.of(context).textTheme.headlineMedium),
+              Text("$message", style: Theme.of(context).textTheme.headlineMedium),
+              ElevatedButton(
+                onPressed: () {
+                  checkIngredients();
+                  cook();
+                },
+                child: Text('Cook'),
+              ),
+
+            ],
           ),
-        ]
-    ),
-  );
-}
-
-// --------------------------------------------
-// Below this line are helper classes and data.
-
-List<Ingredient> availableIngredients = [];
-
-class ConstantScrollBehavior extends ScrollBehavior {
-  const ConstantScrollBehavior();
-
-  @override
-  Widget buildScrollbar(
-      BuildContext context, Widget child, ScrollableDetails details) =>
-      child;
-
-  @override
-  Widget buildOverscrollIndicator(
-      BuildContext context, Widget child, ScrollableDetails details) =>
-      child;
-
-  @override
-  TargetPlatform getPlatform(BuildContext context) => TargetPlatform.macOS;
-
-  @override
-  ScrollPhysics getScrollPhysics(BuildContext context) =>
-      const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics());
+        ),
+        /*
+      floatingActionButton: FloatingActionButton(
+        onPressed: addFlour,
+        tooltip: 'Add Flour',
+        child: const Icon(Icons.add),
+      ),
+       */
+      ),
+    );
+  }
 }
